88b0d0f601180f1241b2999357b4ac729e74d5d4
Toshali Randev <tr4yg@virginia.edu>
2016-02-22 16:38:18 -0500

    Added greeting function to hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-;; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
+__author__ = 'tr4yg'
 
-print("Hello") 
+def greeting(msg):
+    print(msg)
+
+if __name__ == "__main__":
+    greeting("Hello!")
